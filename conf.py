09763230d13b610725ac4f6877aab8be5a7d9f24
Tai @ Flex <dch.tai@gmail.com>
2017-04-11 00:42:04 +0100

    stop displaying Source link

--- conf.py
+++ conf.py
@@ -818,10 +818,10 @@ MARKDOWN_EXTENSIONS = ['fenced_code', 'codehilite', 'extra']
 
 # Show link to source for the posts?
 # Formerly known as HIDE_SOURCELINK (inverse)
-# SHOW_SOURCELINK = True
+SHOW_SOURCELINK = False
 # Copy the source files for your pages?
 # Setting it to False implies SHOW_SOURCELINK = False
-# COPY_SOURCES = True
+COPY_SOURCES = False
 
 # Modify the number of Post per Index Page
 # Defaults to 10
